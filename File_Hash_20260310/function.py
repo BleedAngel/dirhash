a2a530b68a64b4b0f3f0bed933c5d34d2656603d
BleedAngel <aa0918016615@gmail.com>
2026-04-21 17:43:50 +0800

    Update function.py

--- File_Hash_20260310/function.py
+++ File_Hash_20260310/function.py
@@ -12,28 +12,32 @@ def calculate_hash(file_path,algorithm):
     return hash_func.hexdigest()
 
 #存取hash值至txt檔
-def save_hash(file_path,hash_value,hash_file):
-    file_name=os.path.basename(file_path)
-    print(f"✅ Saved hash for: '{file_name}'")
-    with open(hash_file, "a") as f:
-        f.write(f"{file_name},{hash_value}\n")
+def save_hash(file_path,hash_value,hash_file,base_dir):
+    relative_path=os.path.relpath(file_path,base_dir).replace(os.sep, "/")
+    print(f"✅ Saved hash for: '{relative_path}'")
+    with open(hash_file,"a") as f:
+        f.write(f"{relative_path},{hash_value}\n")
 
 #對比hash值
-def verify_hash(file_path,hash_file,algorithm):
+def verify_hash(file_path,hash_file,algorithm,base_dir,summary):
     validate.file(hash_file)
     
     current_hash=calculate_hash(file_path,algorithm)
-    file_name=os.path.basename(file_path)
+    relative_path=os.path.relpath(file_path, base_dir).replace(os.sep, "/")
     with open(hash_file,"r") as f:
         for line in f:
             stored_file,stored_hash=line.strip().split(",")
-            if(stored_file==file_name):
+            stored_file=stored_file.replace("\\", "/")
+            if(stored_file==relative_path):
                 if(stored_hash==current_hash):
-                    print(f"✅ Integrity check passed: '{file_name}'")
-                    return True
+                    print(f"✅ Integrity check passed: '{relative_path}'")
+                    summary["passed"]+=1
+                    return summary
                 else:
-                    print(f"⚠️ Integrity check failed: '{file_name}'")
-                    return False
+                    print(f"⚠️ Integrity check failed: '{relative_path}'")
+                    summary["fail"]+=1
+                    return summary
                 
-    print(f"❌ No record found for: {file_path}")
-    return False
+    print(f"❌ No record found for: {relative_path}")
+    summary["norecord"]+=1
+    return summary
